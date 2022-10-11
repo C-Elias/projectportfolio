c3b5aef9de026c2e3af71f45ac843a1a843c454b
C-Elias <76397063+C-Elias@users.noreply.github.com>
2022-10-11 08:42:51 -0400

    fixed skills section

--- assets/components/skills.js
+++ assets/components/skills.js
@@ -42,7 +42,7 @@ class Skills extends HTMLElement {
           <div class="col-lg-6">
 
             <div class="progress">
-              <span class="skill">Python <i class="val">80%</i></span>
+              <span class="skill">Python <i class="val">30%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
