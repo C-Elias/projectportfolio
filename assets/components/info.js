68c8a2d4ff6bd1459e782d8f40787b578e11718f
C-Elias <76397063+C-Elias@users.noreply.github.com>
2022-11-20 06:40:51 -0500

    updated icons & links

--- assets/components/info.js
+++ assets/components/info.js
@@ -7,22 +7,21 @@ class Info extends HTMLElement {
       this.innerHTML = `
       <section id="hero" class="d-flex flex-column justify-content-center">
       <div class="container" data-aos="zoom-in" data-aos-delay="100">
-        <h1>Elias Chawki</h1>
+        <h1>Elias C.</h1>
         <p>I'm a <span class="typed" data-typed-items="Full Stack Developer, Project Manager, MERN Stack Developer"></span></p>
         <div class="social-links">
         <a href="https://github.com/C-Elias" class="github"><i class="bx bxl-github"></i></a>
         <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
           <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://stackoverflow.com/users/16208207/elias" class="stackoverflow"><i class='bx bxl-stack-overflow'></i></a>
+          <a href="https://dev.to/c_elias" class="stackoverflow"><i class='bx bxl-dev-to'></i></a>
           </div>
           </div>
           </section>
           `;
         }
       }
-      
-      
-      
+
       customElements.define('info-component', Info);
       
       //Extra HTML
